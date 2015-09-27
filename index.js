d5fdfc5f0c70e5f03f11148b7b5ffc2551fd07a1
olivier <guerin.olivier@outlook.com>
2015-09-27 17:56:22 +0200

    improve perf + ...rest

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
 const typeOf = require('./typeof');
 const getConstructorName = require('./getConstructorName');
 
-function TypeOfBuilder(value) {
+function TypeOfBuilder(value, types) {
     /**
      * is an Object ?
      * @type {boolean}
      * @private
      */
-    const _isObj = typeof value === "object";
+    const IS_OBJECT = typeof value === "object";
     /**
      * store type of value
      * @private
      * @type {String}
      */
-    const type = typeOf(value);
+    const VALUE_TYPE = typeOf(value);
 
 
     /**
@@ -28,65 +28,65 @@ function TypeOfBuilder(value) {
      * @private
      */
     function _in(arg) {
-        let _type = typeOf(arg);
-        switch (_type) {
+        let arg_type = typeOf(arg);
+        switch (arg_type) {
             case 'String':
-                if (arg) {
-                    return (arg === 'Object' && type !== 'Array') ? _isObj : type === arg
-                }
-                return type === 'String';
-            case 'Function':
-                return (_isObj) ? value instanceof arg : _in(getConstructorName(arg.prototype));
-            case 'RegExp':
-                return arg.test(type);
-            case 'Array':
-                if (arg.length) {
-                    for (let v of arg) {
-                        if (_in(v)) return true
-                    }
+                if (arg === '') {
+                    return VALUE_TYPE === 'String';
                 } else {
+                    return (arg === 'Object' && VALUE_TYPE !== 'Array') ? IS_OBJECT : VALUE_TYPE === arg;
+                }
+            case 'Function':
+                return (IS_OBJECT === true) ? value instanceof arg : _in(getConstructorName(arg.prototype));
+            case 'RegExp':
+                return arg.test(VALUE_TYPE);
+            case 'Array':
+                if (arg.length === 0) {
                     return _in('Array');
                 }
+                for (let v of arg) {
+                    if (_in(v) === true) return true;
+                }
                 return false;
-            case type:
-                if (_isObj && arg !== null) {
+            case VALUE_TYPE:
+                if (IS_OBJECT === true && arg !== null) {
                     return Object.getPrototypeOf(value) === Object.getPrototypeOf(arg);
                 }
             default:
-                return _in(_type);
+                return _in(arg_type);
         }
     }
 
     /**
      * in case we don't want to create an Object. (can't access this.in & this.getType)
      */
-    if (arguments.length === 2) {
-        return _in(arguments[1]);
+    if (types !== undefined) {
+        return _in(types);
     } else {
         /**
          * call _in function to check if type is among arguments
          * @public
          * @returns {Boolean}
          */
-        this.in = function () {
-            return (arguments.length === 1) ? _in(arguments[0]) : _in(Array.prototype.slice.call(arguments));
+        this.in = function (...types) {
+            return (types.length === 1) ? _in(types[0]) : _in(types);
         };
         /**
          * return type
          * @returns {String}
          */
         this.getType = function () {
-            return type;
+            return VALUE_TYPE;
         };
     }
 
 }
 
-module.exports = function factory(value) {
-    if (arguments.length === 1) {
+module.exports = function factory(value, ...types) {
+    if (types.length === 0) {
         return new TypeOfBuilder(value);
     } else {
-        return (arguments.length === 2) ? TypeOfBuilder(value, arguments[1]) : TypeOfBuilder(value, Array.prototype.slice.call(arguments, 1));
+        return (types.length === 1) ? TypeOfBuilder(value, types[0]) : TypeOfBuilder(value, types);
     }
 };
 
